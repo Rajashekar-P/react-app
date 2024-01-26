ebb46345c80f34f46f57a6b9c264b4501923a816
Rajashekar Pallala <rajashekar@Rajashekars-MacBook-Air.local>
2024-01-26 08:56:10 +0530

    Created signup and login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+const user = {
+  name: 'Rajashekar Pallala',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 
-function App() {
+function Profile() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+function App(){
+  return(
+    <div>
+      <LoginSignup/>
     </div>
   );
 }
